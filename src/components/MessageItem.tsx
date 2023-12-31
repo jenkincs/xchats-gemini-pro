eea9ec059b0690e213e33705d662746bc784b3fd
jenkincsx <jenkincstech@gmail.com>
2023-12-31 18:21:16 +0800

    change the width of the chat box

--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -72,7 +72,7 @@ export default ({ role, message, showRetry, onRetry }: Props) => {
     <div class="py-2 -mx-4 px-4 transition-colors md:hover:bg-slate/3">
       <div class="flex gap-3 rounded-lg" class:op-75={role === 'user'}>
         <div class={`shrink-0 w-7 h-7 mt-4 rounded-full op-80 ${roleClass[role]}`} />
-        <div class="message prose break-words overflow-hidden" innerHTML={htmlString()} />
+        <div class="message prose max-w-fit break-words overflow-hidden" innerHTML={htmlString()} />
       </div>
       {showRetry?.() && onRetry && (
         <div class="fie px-3 mb-2">
